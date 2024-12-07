ab27884e471b987656cc826aca24eda96e72f24b
Mostyle-git <mostyle111@gmail.com>
2024-12-07 17:48:58 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,404 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <title>Mostyle</title>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+  
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="index.php" class="logo"><span>Mostyle</span>&nbsp;The Most Beautiful Style</a>
+
+    <nav class="navbar">
+    <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#service">service</a>
+        <a href="#gallery">gallery</a>
+        <a href="admin/rating/index.php">reivew</a>
+        <a href="#price">Events</a>
+        <a href="#contact">Contact us</a>
+        <a href="Enquiry.php">Enquiry</a>
+        
+        
+        
+    </nav>
+
+    <div id="menu-bars" class="fas fa-bars"></div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3>Mostyle. <span><br> the most beautiful style. </span></h3>
+        <a href="#contact" class="btn">contact us</a>
+    </div>
+
+    <div class="swiper-container home-slider">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide"><img src="images/design/4417.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images/design/4463.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images/design/4597.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images/design/4602.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images/design/4605.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images/design/4606.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images/design/4644.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images/design/4649.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images/design/4651.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images/design/4655.jpg" alt=""></div>
+            <div class="swiper-slide"><img src="images/design/4657.jpg" alt=""></div>
+        </div>
+    </div>
+
+</section>
+
+<!-- home section ends -->
+
+
+
+<!-- about section starts  -->
+
+<section class="about" id="about">
+
+<h1 class="heading"><span>about</span> us </h1>
+
+<div class="row">
+
+    <div class="image">
+        <img src="images/about-img.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>we will give a very special celebration for you</h3>
+        <p>A wedding is a milestone in your life. for you and your loved ones, a day to celebrate and cherish. We, at PN Events mould and shape your vision to design and craft your dream wedding. We aim to make your special day extra special by our expertise finesse and eye for detail. Our experienced team of professionals help you in conceptuilizing, detailing and execution of all the wedding ceremonies.</p>
+        <p>We provide you the best of our resources and contacts, organizational abilities and all the required wedding servies such as decoration, logistics etc. to give you a perfect wedding.
+        PN Events are professionals and work diligently to ensure clients satisfaction.
+        </p>
+        <a href="#contact" class="btn">contact us</a>
+    </div>
+
+</div>
+
+</section>
+
+<!-- about section ends -->
+
+<!-- service section starts  -->
+
+<section class="service" id="service">
+
+    <h1 class="heading"> our <span>services</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box"><a href="services/venue.php">
+            <i class="fas fa-map-marker-alt"></i>
+            <h3>venue selection</h3>
+            <p>Unmatchable location for your living. The place where you live according to your dreams.</p>
+            </a>
+        </div>
+
+        <div class="box"><a href="services/invitation.php">
+            <i class="fas fa-envelope"></i>
+            <h3>invitation card</h3>
+            <p>We may not have it all together, but together we have it all.</p>
+            </a>
+        </div>
+
+        <div class="box"><a href="services/entry.php">
+            <i class="fa fa-diamond"></i>
+            <h3>Entry</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, suscipit.</p>
+            </a>
+        </div>
+
+        
+        <div class="box">
+            <a href="#gallery">
+            <i class="fas fa-photo-video"></i>
+            <h3>photos</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro, suscipit.</p>
+        </a>
+        </div>
+
+        
+
+    </div>
+
+</section>
+
+<!-- service section ends -->
+
+<!-- gallery section starts  -->
+
+<section class="gallery" id="gallery">
+
+    <h1 class="heading">our <span>gallery</span></h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/gallary/4535.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/4554.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/Screenshot_2022-01-11-12-20-14-11_6012fa4d4ddec268fc5c7112cbb265e7.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/Screenshot_2021-12-02-09-36-26-23_1c337646f29875672b5a61192b9010f9.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/Screenshot_2021-10-29-22-44-04-37_6012fa4d4ddec268fc5c7112cbb265e7.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/Screenshot_2021-09-29-20-09-34-49_1c337646f29875672b5a61192b9010f9.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/Screenshot_2021-09-24-08-42-34-54_1c337646f29875672b5a61192b9010f9.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/Screenshot_2021-07-17-21-01-12-83_1c337646f29875672b5a61192b9010f9.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/Screenshot_2021-07-14-11-59-36-06_1c337646f29875672b5a61192b9010f9.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/Screenshot_2021-06-03-23-19-29-89.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/Screenshot_2021-05-17-10-58-05-68.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/IMG20211120145149.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/IMG20220417110314.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/IMG20220216193901.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        <div class="box">
+            <img src="images/gallary/IMG20220217201455_01.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        
+        <div class="box">
+            <img src="images/gallary/IMG20211211190048.jpg" alt="">
+            <h3 class="title">OUR GALLARY</h3>    
+        </div>
+
+        
+
+    </div>
+
+</section>
+
+<!-- gallery section ends -->
+
+<!-- price section starts  -->
+
+<section class="price" id="price">
+
+    <h1 class="heading"> our <span>price</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3 class="title">Birthday Package</h3>
+            <h3 class="amount">55,000</h3>
+            <ul>
+                <li><i class="fas fa-check"></i>full services</li>
+                <li> <i class="fas fa-check"></i> decorations </li>
+                <li> <i class="fas fa-check"></i> music and photos </li>
+                <li> <i class="fas fa-check"></i> food and drinks </li>
+                <li> <i class="fas fa-check"></i> invitation card </li>
+            </ul>
+            <a href="event1.php" class="btn">Book Now</a>
+        </div>
+
+        <div class="box">
+            <h3 class="title">Cradle Ceremony Package</h3>
+            <h3 class="amount">65,500</h3>
+            <ul>
+                <li><i class="fas fa-check"></i>full services</li>
+                <li> <i class="fas fa-check"></i> decorations </li>
+                <li> <i class="fas fa-check"></i> music and photos </li>
+                <li> <i class="fas fa-check"></i> food and drinks </li>
+                <li> <i class="fas fa-check"></i> invitation card </li>
+            </ul>
+            <a href="event1.php" class="btn">Book Now</a>
+        </div>
+
+        <div class="box">
+            <h3 class="title">Naming Ceremony Package</h3>
+            <h3 class="amount">60,000</h3>
+            <ul>
+                <li><i class="fas fa-check"></i>full services</li>
+                <li> <i class="fas fa-check"></i> decorations </li>
+                <li> <i class="fas fa-check"></i> music and photos </li>
+                <li> <i class="fas fa-check"></i> food and drinks </li>
+                <li> <i class="fas fa-check"></i> invitation card </li>
+            </ul>
+            <a href="event1.php" class="btn">Book Now</a>
+        </div>
+
+        <div class="box">
+            <h3 class="title">Wedding Management Package</h3>
+            <h3 class="amount">21,000</h3>
+            <ul>
+                <li><i class="fas fa-check"></i>Hospitality Management</li>
+                <li> <i class="fas fa-check"></i> Production Management </li>
+                <li> <i class="fas fa-check"></i> Technical Management </li>
+                <li> <i class="fas fa-check"></i> F & B Management </li>
+                <li> <i class="fas fa-check"></i> Bride and Groom Shadow </li>
+                <li> <i class="fas fa-check"></i> Bride and Groom Entry </li>
+            </ul>
+            <a href="event1.php" class="btn">Book Now</a>
+        </div>
+
+    </div>
+
+
+</section>
+
+<!-- price section ends -->
+
+<!-- contact section starts  -->
+
+    <section class="contact" id="contact">
+
+    <h1 class="heading"> <span>contact</span> us </h1>
+ 
+    <form method="post" action="admin/contactus/transac.php?action=add">
+        <div class="inputBox">
+            <input type="text" placeholder="Name" name="name">
+            <input type="email" placeholder="E-mail" name="email">
+        </div>
+        <div class="inputBox">
+            <input type="tel" placeholder="Number" name="phone" maxlength="10">
+            <input type="text" placeholder="Address" name="location">
+        </div>
+        <textarea placeholder="your subject" id="" cols="30" rows="10" name="subject"></textarea>
+        <input type="submit" value="Submit" class="btn" name="submit">
+    </form>
+
+    </section>
+
+<!-- contact section ends -->
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="index.php #home"> <i class="fas fa-arrow-right"></i> home </a>
+            <a href="#service"> <i class="fas fa-arrow-right"></i> service </a>
+            <a href="#about"> <i class="fas fa-arrow-right"></i> about </a>
+            <a href="#gallery"> <i class="fas fa-arrow-right"></i> gallery </a>
+            <a href="#contact"> <i class="fas fa-arrow-right"></i> contact </a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="https://wa.me/<9834827302>"> <i class="fas fa-phone"></i> +91-9834827302 </a>
+            <a href="tel:+918007804952"> <i class="fas fa-phone"></i> +91-8007804952 </a>
+            <a href=" mailto:narkhedeprasad8@gmail.com"> <i class="fas fa-envelope"></i> narkhedeprasad8@gmail.com </a>
+            <a href="https://goo.gl/maps/gh9v1571TLZUAq2A9"> <i class="fas fa-map-marker-alt"></i> Jalgaon, India - 425001 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/Celebrino-PRN-Events-2262924940396832"> <i class="fab fa-facebook-f"></i> facebook </a>
+
+            <a href="https://instagram.com/pn___events?utm_medium=copy_link"> <i class="fab fa-instagram"></i> instagram </a>
+            
+        </div>
+
+    </div>
+
+    
+
+</section>
+
+<!-- footer section ends -->
+
+<!-- theme toggler  -->
+
+<div class="theme-toggler">
+
+    <div class="toggle-btn">
+        <i class="fas fa-cog"></i>
+    </div>
+
+    <h3>choose color</h3>
+
+    <div class="buttons">
+        <div class="theme-btn" style="background: #3867d6;"></div>
+        <div class="theme-btn" style="background: #f7b731;"></div>
+        <div class="theme-btn" style="background: #ff0033;"></div>
+        <div class="theme-btn" style="background: #20bf6b;"></div>
+        <div class="theme-btn" style="background: #fa8231;"></div>
+        <div class="theme-btn" style="background: #FC427B;"></div>
+    </div>
+
+</div>
+
+
+
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+ 
+
+</body>
+</html>
