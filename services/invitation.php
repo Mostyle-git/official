b808a466ec244c502865e03cbaf1d899fb9c4283
Mostyle-git <mostyle111@gmail.com>
2024-12-07 18:57:53 +0530

    Add files via upload

---
+++ services/invitation.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+<body>
+  
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="../index.php" class="logo"><span>Event</span>&nbsp;Portfolio</a>
+
+    <nav class="navbar">
+        <a href="../index.php #home">home</a>
+        <a href="../index.php #service">service</a>
+        <a href="../index.php #about">about</a>
+        <a href="../index.php #gallery">gallery</a>
+        <a href="../index.php #events">Events</a>
+        <a href="../index.php #contact">Contact us</a>
+        
+        
+    </nav>
+
+    <div id="menu-bars" class="fas fa-bars"></div>
+
+</header>
+
+<!-- header section ends -->
+
+
+
+<!-- gallery section starts  -->
+
+<section class="gallery" id="gallery">
+
+    <h1 class="heading">Invitation <span>Samples</span></h1>
+
+    <div class="box-container"> 
+
+        <div class="box">
+            <img src="../images/invitation/g-1.jpg" alt="">
+            <h3 class="title">Invitation <span> Samples </span></h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/invitation/g-2.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/invitation/g-3.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3> 
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/invitation/g-4.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/invitation/g-5.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/invitation/g-6.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/invitation/g-7.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/invitation/g-8.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/invitation/g-9.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/invitation/g-15.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3>
+            
+        </div>
+        
+        <div class="box">
+            <img src="../images/invitation/g-11.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3>
+            
+        </div>
+        
+        <div class="box">
+            <img src="../images/invitation/g-12.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/invitation/g-13.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/invitation/g-15.jpg" alt="">
+            <h3 class="title">Invitation<span>Samples </span></h3>
+            
+        </div>
+
+    </div>
+
+</section>
+
+<!-- gallery section ends -->
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="index.php #home"> <i class="fas fa-arrow-right"></i> home </a>
+            <a href="#service"> <i class="fas fa-arrow-right"></i> service </a>
+            <a href="#about"> <i class="fas fa-arrow-right"></i> about </a>
+            <a href="#gallery"> <i class="fas fa-arrow-right"></i> gallery </a>
+            <a href="#contact"> <i class="fas fa-arrow-right"></i> contact </a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="https://wa.me/<9834827302>"> <i class="fas fa-phone"></i> +91-9834827302 </a>
+            <a href="tel:+918007804952"> <i class="fas fa-phone"></i> +91-8007804952 </a>
+            <a href=" mailto:narkhedeprasad8@gmail.com"> <i class="fas fa-envelope"></i> narkhedeprasad8@gmail.com </a>
+            <a href="https://goo.gl/maps/gh9v1571TLZUAq2A9"> <i class="fas fa-map-marker-alt"></i> Jalgaon, India - 425001 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/Celebrino-PRN-Events-2262924940396832"> <i class="fab fa-facebook-f"></i> facebook </a>
+
+            <a href="https://instagram.com/pn___events?utm_medium=copy_link"> <i class="fab fa-instagram"></i> instagram </a>
+            
+        </div>
+
+    </div>
+
+    
+
+</section>
+
+<!-- footer section ends -->
+
+<!-- theme toggler  -->
+
+<div class="theme-toggler">
+
+    <div class="toggle-btn">
+        <i class="fas fa-cog"></i>
+    </div>
+
+    <h3>choose color</h3>
+
+    <div class="buttons">
+        <div class="theme-btn" style="background: #3867d6;"></div>
+        <div class="theme-btn" style="background: #f7b731;"></div>
+        <div class="theme-btn" style="background: #ff0033;"></div>
+        <div class="theme-btn" style="background: #20bf6b;"></div>
+        <div class="theme-btn" style="background: #fa8231;"></div>
+        <div class="theme-btn" style="background: #FC427B;"></div>
+    </div>
+
+</div>
+
+
+
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<!-- custom js file link  -->
+<script src="js/script.js"></script>  
+
+</body>
+</html>
---
+++ services/venue.php
@@ -0,0 +1,245 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+<body>
+  
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="../index.php" class="logo"><span>Event</span>&nbsp;Portfolio</a>
+
+    <nav class="navbar">
+        <a href="../index.php">home</a>
+        <a href="../index.php #service">service</a>
+        <a href="../index.php #about">about</a>
+        <a href="../index.php #gallery">gallery</a>
+        <a href="./index.php #events">Events</a>
+        <a href="../enquiry.php">enquiry</a>
+        
+    </nav>
+
+    <div id="menu-bars" class="fas fa-bars"></div>
+
+</header>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+    <div class="content">
+        <h3>Venue <span>Selection </span></h3>
+       
+    </div>
+
+    
+
+</section>
+
+<!-- home section ends -->
+
+
+
+<!-- gallery section starts  -->
+
+<section class="gallery" id="gallery">
+
+    <h1 class="heading">For <span>Weddings</span></h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="../images/images(12).jpeg" alt="">
+            <h3 class="title">Hotel Tanarika Resort</h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/images (7).jpeg" alt="">
+            <h3 class="title">Hotel four Seasons Hall</h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/images (11).jpeg" alt="">
+            <h3 class="title">Hotel President Cottage Lawn</h3> 
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/OIP.jpg" alt="">
+            <h3 class="title">Hotel President Cottage Hall</h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/images (10).jpeg" alt="">
+            <h3 class="title">Hotel four Seasons Lawn</h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/images (8).jpeg" alt="">
+            <h3 class="title">Kamal Paradise Hall</h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/download.jpg" alt="">
+            <h3 class="title">Kamal Paradise Lawn</h3>
+            
+        </div>
+
+    </div>
+
+</section>
+
+
+<section class="gallery" id="gallery">
+
+    <h1 class="heading">For <span>Birthdays</span></h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="../images/event/1c49f9072c01895704897a7fb866c5de.jpg" alt="">
+            <h3 class="title">@ your Location</h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/event/2e94c09d212ec2f9d3bf600f021b963e.jpg" alt="">
+            <h3 class="title">Silver Palace</h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/event/3d0be6d37057c35735823f8fb04d4af1.jpg" alt="">
+            <h3 class="title">Hotel Grappies</h3> 
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/event/3e43352c3c6d856e79bca81c3212f79e.jpg" alt="">
+            <h3 class="title">Kamal Paradise</h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/event/3f9b3408ee345e08c3a02ebe58445a44.jpg" alt="">
+            <h3 class="title">Hotel four Seasons Lawn</h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/event/6ae8d3cc8c9d34bdcb11f2e953f4ab36.jpg" alt="">
+            <h3 class="title">K.P Pride</h3>
+            
+        </div>
+
+        <div class="box">
+            <img src="../images/event/6489d0aa7b4b40c2f918334c740a67d1.jpg" alt="">
+            <h3 class="title">Cafe Crush</h3>
+            
+        </div>
+        
+    </div>
+
+</section>
+
+<!-- gallery section ends -->
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>branches</h3>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> mumbai </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> jogeshwari </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> goregaon </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> navi mumbai </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> andheri </a>
+        </div>
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#"> <i class="fas fa-arrow-right"></i> home </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> service </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> about </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> gallery </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> price </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> reivew </a>
+            <a href="#"> <i class="fas fa-arrow-right"></i> contact </a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +91-9834827302 </a>
+            <a href="#"> <i class="fas fa-phone"></i> +91-8007804952 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> narkhedeprasad8@gmail.com </a>
+            <a href="#"> <i class="fas fa-map-marker-alt"></i> Jalgaon, India - 425001 </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://www.facebook.com/Celebrino-PRN-Events-2262924940396832"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="https://instagram.com/pn___events?utm_medium=copy_link"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span>Prasad Narkhede</span> | P.N Evento </div>
+
+</section>
+
+<!-- footer section ends -->
+
+<!-- theme toggler  -->
+
+<div class="theme-toggler">
+
+    <div class="toggle-btn">
+        <i class="fas fa-cog"></i>
+    </div>
+
+    <h3>choose color</h3>
+
+    <div class="buttons">
+        <div class="theme-btn" style="background: #3867d6;"></div>
+        <div class="theme-btn" style="background: #f7b731;"></div>
+        <div class="theme-btn" style="background: #ff0033;"></div>
+        <div class="theme-btn" style="background: #20bf6b;"></div>
+        <div class="theme-btn" style="background: #fa8231;"></div>
+        <div class="theme-btn" style="background: #FC427B;"></div>
+    </div>
+
+</div>
+
+
+
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+<!-- custom js file link  -->
+<script src="js/script.js"></script>  
+
+</body>
+</html>
